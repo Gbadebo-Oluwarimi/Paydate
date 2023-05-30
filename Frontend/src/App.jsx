8e1dd4aca63d0f19ca97f2121d6a6af69c2ade7d
Gbadebo Oluwarimi <oluwarimigbadebo@gmail.com>
2023-05-29 17:51:37 -0700

    Stuff worked easily

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Userpage from './Mainpage/Userpage';
 import Register from './Authroutes/Register';
 // create a apollo server connection here 
 
--- Frontend/src/Authroutes/Register.jsx
+++ Frontend/src/Authroutes/Register.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { gql } from '@apollo/client'
+import { useMutation } from '@apollo/client'
+import { useState } from 'react'
+
+
+const LOGIN_USER = gql`
+mutation logins($loginInput: LoginInput){
+    LoginUser(loginInput: $loginInput) {
+      email
+    }
+  }
+`
+
 
 const Register = () => {
+
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [LoginUser, { loading, error }] = useMutation(LOGIN_USER);
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        LoginUser({
+            variables: {
+              loginInput: {
+                password,
+                email,
+              },
+            },
+          })
+         .then((response) => {
+              // Handle successful response
+              console.log(response);
+            })
+         .catch((error) => {
+              // Handle error
+              console.error(error);
+            });
+    }
   return (
     <div>
       Kindly Register
+      <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <button type="submit">Submit</button>
+    </form>
     </div>
   )
 }
